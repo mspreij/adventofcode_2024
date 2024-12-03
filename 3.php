7861af068ba52009fb13854958e7f44050be8af6
Maarten Spreij <maarten@fargeau.nl>
2024-12-03 10:27:25 +0100

    day 3b

--- 3.php
+++ 3.php
@@ -4,12 +4,25 @@ set_time_limit(1);
 
 $data = file_get_contents('3.txt');
 
-//$data = 'xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))';
-preg_match_all('/(mul\(\d+,\d+\))/m', $data, $matches);
+// $data = 'xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))';
+// $data = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+preg_match_all('/(mul\(\d+,\d+\)|do\(\)|don\'t\(\))/m', $data, $matches);
 
+$add = true;
 $result = 0;
 foreach ($matches[1] as $instruction) {
-    list($a, $b) = explode(',', substr($instruction, 4, -1));
-    $result += $a * $b;
+    $type = substr($instruction, 0, 3);
+    if ($add) {
+        if ($type == 'mul') {
+            list($a, $b) = explode(',', substr($instruction, 4, -1));
+            $result += $a * $b;
+        }elseif ($type == 'don') {
+            $add = false;
+        }
+    }else{
+        if ($type == 'do(') {
+            $add = true;
+        }
+    }
 }
 echo $result;
